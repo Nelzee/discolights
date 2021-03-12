5ae715a85866b28c97aaa3d651cea7746a37030b
Nelzee <38656339+Nelzee@users.noreply.github.com>
2021-03-12 14:43:31 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,17 @@ const bck = document.querySelector(".container");
 
 const randomColor = () => {
   let color = 'rgba(';
-  for(let i = 0;i < 3;i++){
-
-   color += Math.floor(Math.random() * 255) + ',';
+  for(let i = 0;i< 3;i++){
+    color += Math.floor(Math.random() * 255) + ',';
   }
   return color;
-};
+}
 
-const rand = () => {
+const paint = () => {
   clr = randomColor();
   clr1 = randomColor();
-  document.documentElement.style.setProperty('--clr', clr +'1)');
-  document.documentElement.style.setProperty('--clr1', clr1 +'1)');
+  document.documentElement.style.setProperty('--clr', clr + '1)');
+  document.documentElement.style.setProperty('--clr1', clr1 + '1)');
 };
 
-setInterval(rand, 50);
+setInterval(paint, 100);
